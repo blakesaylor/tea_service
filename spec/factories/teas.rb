a5de9fa7e56341a2b289d2ee61a92fa75e114ad9
Blake Saylor <Blakesaylor@gmail.com>
2022-11-01 14:39:34 -0600

    Create and test endpoint for Customer Subscriptions index

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :tea do
-    name { "MyString" }
-    description { "MyString" }
-    temperature_c { 1 }
-    brew_time_sec { 1 }
-    price { 1.5 }
+    name { Faker::Tea.variety }
+    description { Faker::Food.description }
+    temperature_c { Faker::Number.between(from: 120, to: 210) }
+    brew_time_sec { Faker::Number.between(from: 60, to: 240) }
+    price { Faker::Number.decimal(l_digits: 1, r_digits: 2) }
   end
 end
