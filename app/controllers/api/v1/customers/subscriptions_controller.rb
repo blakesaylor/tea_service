422c164a5b45ff9b5173689c1d086be31b440134
Blake Saylor <Blakesaylor@gmail.com>
2022-11-02 10:04:56 -0600

    Create endpoint and tests for cancelling Subscription (without deleting)

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -23,7 +23,40 @@ class Api::V1::Customers::SubscriptionsController < ApplicationController
       end
       render json: SubscriptionSerializer.new(subscription), status: :created
     else
-      render json: { description: "Error: Invalid parameters"}, status: :not_found
+      render json: { description: "Error: Invalid parameters" }, status: :not_found
+    end
+  end
+
+  def update
+    subscription = Subscription.find_by_id(params[:id])
+    if subscription.present?
+      if params[:change] == 'cancel'
+        cancel_subscription(subscription)
+      # elsif restart_subscription
+        # Some stuff to restart a subscription
+      # elsif add_tea
+        # Some stuff here to add a tea
+      # elsif remove_tea
+        # Some stuff here to remove a tea
+      # elsif change_frequency
+        # Some stuff here to change frequency
+      end
+    else
+      render json: { description: "Error: Invalid parameters" }, status: :not_found
+    end
+  end
+
+  private
+  # def valid_update_param?
+  #   ['cancel', 'restart_subscription', 'add_tea', 'remove_tea', 'change_frequency'].include?(params[:change])
+  # end
+
+  def cancel_subscription(subscription)
+    if subscription.active?
+      subscription.cancelled!
+      render json: SubscriptionSerializer.new(subscription), status: :ok
+    else
+      render json: { description: "Error: Subscription is already cancelled" }, status: :not_found
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :customer, only: [] do
-        resources :subscriptions, only: [ :index, :create ], controller: 'customers/subscriptions'
+        resources :subscriptions, only: [ :index, :create, :update ], controller: 'customers/subscriptions'
       end
     end
   end
